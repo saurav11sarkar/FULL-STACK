7205102221fc47d84b70079538ff04a02a7f8c43
saurav11sarkar <sarkarsaurav11@outlook.com>
2024-11-17 21:35:43 +0600

    mongoose-1

---
+++ mongoose-1/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send('Hello worlds');
+});
+
+export default app;
---
+++ mongoose-1/src/server.ts
@@ -0,0 +1,18 @@
+import app from "./app";
+import config from "./app/config";
+import mongoose from 'mongoose';
+
+const mailDB = async () => {
+    try {
+        await mongoose.connect(config.DATABASE_URL as string)
+        app.listen(config.PORT, () => {
+            console.log(`Server is running http://localhost:${config.PORT}`)
+        })
+        console.log('Database is connection')
+    } catch (error) {
+        console.log(error)
+    }
+}
+mailDB();
+
+
