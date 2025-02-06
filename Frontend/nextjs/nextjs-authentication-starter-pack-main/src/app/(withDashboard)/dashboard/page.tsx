b1b4089d848d0a683b485cdcb0417a216555f08a
saurav11sarkar <sarkarsaurav11@outlook.com>
2025-02-06 17:57:33 +0600

    nextjs

--- Frontend/nextjs/nextjs-authentication-starter-pack-main/src/app/(withDashboard)/dashboard/page.tsx
+++ Frontend/nextjs/nextjs-authentication-starter-pack-main/src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,29 @@
-const DashboardPage = () => {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/components/utils/authOptions";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log("DashboardPage", session);
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+      {session?.user && (
+        <>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome To {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            Login User email {session?.user?.email}
+          </h1>
+          <Image
+            className={"mx-auto rounded-full mt-5"}
+            src={session?.user?.image || "https://img.icons8.com/?size=100&id=108652&format=png&color=000000"}
+            alt={session?.user?.name as string}
+            width={100}
+            height={100}
+          />
+        </>
+      )}
     </div>
   );
 };
--- Frontend/nextjs/nextjs-authentication-starter-pack-main/src/app/api/auth/[...nextauth]/route.ts
+++ Frontend/nextjs/nextjs-authentication-starter-pack-main/src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth"
+import {authOptions} from "@/components/utils/authOptions";
 
-const handler = NextAuth({
-    ...
-})
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
